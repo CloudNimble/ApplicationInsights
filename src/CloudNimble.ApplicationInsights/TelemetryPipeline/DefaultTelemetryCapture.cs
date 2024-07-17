d8dd8177e36e643030603445331a48e1ec166122
Robert McLaws <1657085+robertmclaws@users.noreply.github.com>
2024-07-17 01:07:33 -0400

    Figured out how to get an Operation ID. User and Session are next.

--- src/CloudNimble.ApplicationInsights/TelemetryPipeline/DefaultTelemetryCapture.cs
+++ src/CloudNimble.ApplicationInsights/TelemetryPipeline/DefaultTelemetryCapture.cs
@@ -1,9 +1,6 @@
 ﻿using CloudNimble.ApplicationInsights.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
+using System.Diagnostics;
 using System.Reflection;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace CloudNimble.ApplicationInsights.TelemetryPipeline
@@ -24,6 +21,7 @@ namespace CloudNimble.ApplicationInsights.TelemetryPipeline
         /// <returns></returns>
         public async Task CaptureAsync<T>(RequestDetails<T> details) where T : InsightsBase
         {
+            details.Tags.TryAdd("ai.operation.id", ActivitySpanId.CreateRandom().ToHexString());
             details.Tags.TryAdd("ai.internal.sdkVersion", $"net8:{Assembly.GetExecutingAssembly()
                               .GetCustomAttribute<AssemblyInformationalVersionAttribute>()
                               ?.InformationalVersion ?? "1.0.0"}");
